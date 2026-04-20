91c328bcade4c9277a5eb498617b671865a461b8
dahai262626 <153343267+dahai262626@users.noreply.github.com>
2026-04-20 13:26:22 +0800

    Update uart_vrf_component.h

--- components/uart_vrf/uart_vrf_component.h
+++ components/uart_vrf/uart_vrf_component.h
@@ -6,28 +6,27 @@
 
 namespace vrf_protocol { // Forward declaration
   class VrfCmd;
-  class VrfGateway; 
+  class VrfGateway;
+  class VrfClimate;
 }
 #include "vrf.h"
 
 namespace esphome {
 namespace uart_vrf {
 
-class UartVrfClimate; 
+class UartVrfClimate;
 
 class VrfGatewayWrapper {
-
   public:
-  void add_gateway(vrf_protocol::VrfGateway* gateway);
+  void add_gateway(vrf_protocol::VrfGateway *gateway);
   void consume_data(uint8_t data);
   vrf_protocol::VrfCmd cmd_find_climates();
   vrf_protocol::VrfCmd cmd_query_next_climate();
   std::vector<vrf_protocol::VrfClimate *> get_climates();
 
   private:
-  // 目标
-  vrf_protocol::VrfGateway* vrf_gateway_{nullptr};
-  std::vector<vrf_protocol::VrfGateway*> gateways_;
+  vrf_protocol::VrfGateway *vrf_gateway_{nullptr};
+  std::vector<vrf_protocol::VrfGateway *> gateways_;
   uint8_t next_idx_{0};
 
   uint8_t get_next_idx();
@@ -35,21 +34,30 @@ class VrfGatewayWrapper {
 };
 
 class UartVrfComponent : public Component, public uart::UARTDevice {
-public:
+ public:
   UartVrfComponent(uart::UARTComponent *uartComponent) : Component(), UARTDevice(uartComponent) {
     this->uart_ = uartComponent;
-  };
+  }
   void setup() override;
   void loop() override;
   void send_cmd(vrf_protocol::VrfCmd cmd);
-  uart::UARTComponent* get_uart() { return this->uart_; };
-  void on_climate_create_callback(vrf_protocol::VrfClimate* climate);
-  void on_climate_state_callback(vrf_protocol::VrfClimate* climate);
+  uart::UARTComponent *get_uart() { return this->uart_; }
 
-protected:
-  uart::UARTComponent* uart_;
-  VrfGatewayWrapper* vrf_gateway_wrapper_;
-  std::vector<UartVrfClimate*> climates_;
+  // Called from climate.py codegen at compile time to register pre-declared
+  // climate slots. Runtime-discovered VRF units are bound to these slots in order.
+  void add_child_climate(UartVrfClimate *climate) { this->child_climates_.push_back(climate); }
+
+  // Runtime callbacks fired by the VRF protocol layer.
+  void on_climate_create_callback(vrf_protocol::VrfClimate *climate);
+  void on_climate_state_callback(vrf_protocol::VrfClimate *climate);
+
+ protected:
+  uart::UARTComponent *uart_;
+  VrfGatewayWrapper *vrf_gateway_wrapper_;
+  // Pre-declared UartVrfClimate entities from YAML, registered via add_child_climate().
+  std::vector<UartVrfClimate *> child_climates_;
+  // How many child slots have been bound to a discovered VRF unit so far.
+  size_t bound_count_{0};
   std::vector<std::vector<uint8_t>> pending_cmds_;
   unsigned long last_time_heartbeat_cmds_{0};
   unsigned long last_time_fire_cmd{0};
@@ -57,8 +65,7 @@ protected:
   void fire_cmd();
   void find_climates();
   void query_next_climate();
-  
 };
 
-}
-}
+} // namespace uart_vrf
+} // namespace esphome
