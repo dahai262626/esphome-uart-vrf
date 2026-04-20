d87b2c9ed9833ec9819aada890dd64fbc1e75255
dahai262626 <153343267+dahai262626@users.noreply.github.com>
2026-04-20 13:24:36 +0800

    Update climate.py

--- components/uart_vrf/climate.py
+++ components/uart_vrf/climate.py
@@ -2,31 +2,30 @@ import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.components import climate
 from esphome.const import CONF_ID
-from .. import UartVrfComponent
+from . import UartVrfComponent, uart_vrf_ns
 
 DEPENDENCIES = ['uart_vrf']
 
-uart_vrf_ns = cg.esphome_ns.namespace('uart_vrf')
-uartVrfClimate = uart_vrf_ns.class_('UartVrfClimate', 
-    climate.Climate, 
-    cg.Component, 
+UartVrfClimate = uart_vrf_ns.class_(
+    'UartVrfClimate',
+    climate.Climate,
+    cg.Component,
     cg.Parented.template(UartVrfComponent),
 )
 
-CONFIG_SCHEMA = climate.CLIMATE_SCHEMA.extend({
-    cv.GenerateID(): cv.declare_id(uartVrfClimate),
+CONFIG_SCHEMA = climate.climate_schema(UartVrfClimate).extend({
     cv.GenerateID("uart_vrf_id"): cv.use_id(UartVrfComponent),
-    cv.Optional("bind_climate_id"): cv.use_id(climate)
 }).extend(cv.COMPONENT_SCHEMA)
 
-def to_code(config):
-    paren = yield cg.get_variable(config["uart_vrf_id"])
+
+async def to_code(config):
+    paren = await cg.get_variable(config["uart_vrf_id"])
+    # new_Pvariable with no extra args - UartVrfClimate now has a default constructor.
+    # The VrfClimate link is set at runtime in on_climate_create_callback.
     var = cg.new_Pvariable(config[CONF_ID])
-    yield cg.register_parented(var, paren)
-    yield cg.register_component(var, config)
-    yield climate.register_climate(var, config)
-    
-    # cg.add(paren.register_homekit_entities(var))
-    # if "bind_climate_id" in config:
-    #     bind_climate = yield cg.get_variable(config["bind_climate_id"])
-    #     cg.add(var.set_bind_climate(bind_climate))
+    await cg.register_parented(var, paren)
+    await cg.register_component(var, config)
+    await climate.register_climate(var, config)
+    # Register this climate as a child slot on the parent UartVrfComponent,
+    # so at runtime discovered VRF units can be bound to it.
+    cg.add(paren.add_child_climate(var))
