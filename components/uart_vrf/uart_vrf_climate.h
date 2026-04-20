9aeceb4372717437827ff1c29b73dada85120840
dahai262626 <153343267+dahai262626@users.noreply.github.com>
2026-04-20 13:25:36 +0800

    Update uart_vrf_climate.h

--- components/uart_vrf/uart_vrf_climate.h
+++ components/uart_vrf/uart_vrf_climate.h
@@ -11,20 +11,22 @@ class UartVrfComponent;
 
 class UartVrfClimate : public Component, public climate::Climate, public Parented<UartVrfComponent> {
     public:
-    UartVrfClimate(vrf_protocol::VrfClimate* core_climate) {
-        this->core_climate_ = core_climate;
-    }
-    void setup() override;
-    void dump_config() override;
+    // Default constructor – required by the new codegen path. The VrfClimate
+    // pointer is set later at runtime when a VRF unit is discovered.
+    UartVrfClimate() = default;
+
+    void setup() override {}
+    void dump_config() override {}
     void control(const climate::ClimateCall &call) override;
     climate::ClimateTraits traits() override;
-    vrf_protocol::VrfClimate* get_core_climate() { return this->core_climate_; };
+
+    // Runtime binding: link a pre-declared UartVrfClimate to a discovered VRF unit.
+    void set_core_climate(vrf_protocol::VrfClimate *core_climate) { this->core_climate_ = core_climate; }
+    vrf_protocol::VrfClimate *get_core_climate() { return this->core_climate_; }
+    bool is_bound() const { return this->core_climate_ != nullptr; }
 
     protected:
-    vrf_protocol::VrfClimate* core_climate_;
-
-    private:
-
+    vrf_protocol::VrfClimate *core_climate_{nullptr};
 };
 
 } // namespace uart_vrf
